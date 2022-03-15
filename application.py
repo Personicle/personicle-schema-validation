5d0bdcf99cfdea05c1ce52e4ab6cc4a42825c74b
Vaibhav Pandey <vaibhap1@uci.edu>
2022-03-14 22:53:33 -0700

    implemented data validation end points

--- application.py
+++ application.py
@@ -5,30 +5,31 @@ from fastavro import validate, parse_schema
 import json
 from data_dictionary import find_datastream, find_event_definition
 
-Log_Format = "%(levelname)s %(asctime)s - %(message)s"
-logging.basicConfig(filename = "data_dictionary.log",
-                    filemode = "w",
-                    format = Log_Format,
-                    level=logging.DEBUG
-                    )
-logger = logging.getLogger(__name__) 
+# Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+# logging.basicConfig(filename = "data_dictionary.log",
+#                     filemode = "w",
+#                     format = Log_Format,
+#                     level=logging.DEBUG
+#                     )
+# logger = logging.getLogger(__name__) 
 
 script_dir = os.path.dirname(__file__)
 
-app = Flask()
+app = Flask(__name__)
 
 @app.route("/")
 def test_application():
-    return Response("Testing schema validation application")
+    # logger.info("Testing the application")
+    return "Testing schema validation application"
 
 
-@app.route("/validate-data-packet", ["POST"])
+@app.route("/validate-data-packet", methods=["POST"])
 def validate_data_packet():
     data_type = request.args.get("data_type")
-    current_event = request.get_json()['data']
+    current_event = request.get_json()
     # current_event = event.body_as_json(encoding='UTF-8')
     try:
-        logger.info("Received data packet for data type {}:\n{}".format(data_type, json.dumps(current_event, indent=2)))
+        # logger.info("Received data packet for data type {}:\n{}".format(data_type, json.dumps(current_event, indent=2)))
 
         stream_type = current_event['streamName']
         
@@ -38,9 +39,10 @@ def validate_data_packet():
         elif data_type == "datastream":
             stream_information = find_datastream(stream_type)
         else:
-            logger.warn("Incorectly formatted data, data type not found in data dictionary: {}".format(json.dumps(current_event, indent=2)))
+            # logger.warn("Incorectly formatted data, data type not found in data dictionary: {}".format(json.dumps(current_event, indent=2)))
             return Response("Incorectly formatted data, data type not found in data dictionary", 403)
-        
+        if stream_information is None:
+            return Response("stream type not found", 404)
 
         # stream_information = match_data_dictionary(stream_type)
         # get the corresponding schema and the table
@@ -52,8 +54,10 @@ def validate_data_packet():
         # validate the event with avro schema
         parsed_schema = parse_schema(schema) 
         if validate(current_event, parsed_schema, raise_errors=False):
-            logger.info("Valid event")
-            return Response({"schema_check": True})
+            # logger.info("Valid event")
+            return jsonify({"schema_check": True})
+        else:
+            return jsonify({"schema_check": False})
     except KeyError:
         return Response("Incorrect packet format", 403)
 
@@ -61,12 +65,22 @@ def validate_data_packet():
 @app.route("/match-data-dictionary")
 def match_data_dictionary():
     data_type = request.args.get("data_type")
+    print("data type is {}".format(data_type))
     stream_type = request.args.get("stream_name")
+    print("stream name is {}".format(stream_type))
     if data_type == "event":
         stream_information = find_event_definition(stream_type)
     elif data_type == "datastream":
         stream_information = find_datastream(stream_type)
     else:
-        logger.warn("Incorectly formatted data, data type not found in data dictionary: {}".format(json.dumps(current_event, indent=2)))
-        return Response("Incorectly formatted data, data type not found in data dictionary", 403)
-    return jsonify(stream_information)
+        # logger.warn("Incorectly formatted data, data type not found in data dictionary: {}".format(stream_type))
+        return Response("Incorectly formatted data, data type must be event or datastream", 403)
+    if stream_information:
+        return jsonify(stream_information)
+    else:
+        return Response("Stream type not found", 403)
+
+
+if __name__ == "__main__":
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    app.run("localhost", port=5000, debug=True)
